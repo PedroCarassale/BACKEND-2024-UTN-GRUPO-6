170735b09ffef1bcef7753b6b23e9e916500c166
rodri <rodrigoramirezok@hotmail.com>
2024-10-01 23:15:01 -0300

    Se agregaron 2 request gimnasio get getAll y getById

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GimnasioModule } from './gimnasio/gimnasio.module';
 import { GimnasioController } from './gimnasio/gimnasio.controller';
+import { GimnasioService } from './gimnasio/gimnasio.service';
 
 @Module({
   imports: [GimnasioModule],
   controllers: [GimnasioController],
-  providers: [],
+  providers: [GimnasioService],
 })
 export class AppModule {}
--- src/gimnasio/gimnasio.controller.ts
+++ src/gimnasio/gimnasio.controller.ts
@@ -1,9 +1,22 @@
-import {Controller, Get} from "@nestjs/common";
+import {Controller, Get, Param} from "@nestjs/common";
+import { GimnasioService } from "./gimnasio.service"; // Injecto GimnasioService en el Controller para utilizar sus funciones.
 
 @Controller({})
-export class GimnasiosController {
+export class GimnasioController {
+    gimnasioService:GimnasioService; // Declaro la variable en Controller.
+    constructor(gimnasioService:GimnasioService){
+        this.gimnasioService=gimnasioService; // Igualo mi variable local con la recibida por parametro.
+    }
+
     @Get("/gimnasios")
-    getGimnasios() {
-        return ("<h1>GimnasiosID</h1>");
+    async getAll(){
+        const result = await this.gimnasioService.getAll();
+        return result;
+    }
+
+    @Get("/gimnasio:id")
+    async getById(@Param("id") id) {
+        const result = await this.gimnasioService.getById(id);
+        return (result);
     }
 }
