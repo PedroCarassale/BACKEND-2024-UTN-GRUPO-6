2609ab336afa3e6af0fead172cac69d9f1780eef
PedroCarassale <pcarassale@decampoacampo.dev>
2024-10-04 14:11:02 -0300

    use gym; en la config de la base de datos

--- bd_mysql_export/bd_gym.sql
+++ bd_mysql_export/bd_gym.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `_prisma_migrations`
 --
 
+USE gym;
+
 DROP TABLE IF EXISTS `_prisma_migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
