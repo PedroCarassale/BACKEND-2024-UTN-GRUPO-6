6e5167525ca021aaaf458b15bff6ada28ac8e1f6
rodri <rodrigoramirezok@hotmail.com>
2024-10-07 09:54:23 -0300

    fds

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Especifica el origen exacto del frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Si necesitas enviar cookies o autenticación
+  });
   //Es un pipe que viene por defecto en NestJS para aplicar validaciones a los datos que llegan en las solicitudes. Cuando se usa @UsePipes(new ValidationPipe()), NestJS toma el DTO que defines en el controlador y valida los datos entrantes contra las reglas especificadas en el DTO mediante class-validator.
   app.useGlobalPipes(new ValidationPipe()) // Declarandolo aqui me evito estar repitiendo siempre la misma linea de codigo y me aseguro que todas las rutas tengan validaciones, siempre y cuando tengan un DTO.
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
-  app.enableCors();
 }
 bootstrap();
