c7f5b4048b519ca2e8824cc5cf3dc5489e9235c4
PedroCarassale <pcarassale@decampoacampo.dev>
2024-11-10 12:00:39 -0300

    limite para imagenes aumentado

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,13 @@ async function bootstrap() {
     credentials: true, // Si necesitas enviar cookies o autenticación
   });
   //Es un pipe que viene por defecto en NestJS para aplicar validaciones a los datos que llegan en las solicitudes. Cuando se usa @UsePipes(new ValidationPipe()), NestJS toma el DTO que defines en el controlador y valida los datos entrantes contra las reglas especificadas en el DTO mediante class-validator.
-  app.useGlobalPipes(new ValidationPipe()) // Declarandolo aqui me evito estar repitiendo siempre la misma linea de codigo y me aseguro que todas las rutas tengan validaciones, siempre y cuando tengan un DTO.
+  app.useGlobalPipes(new ValidationPipe()); // Declarandolo aqui me evito estar repitiendo siempre la misma linea de codigo y me aseguro que todas las rutas tengan validaciones, siempre y cuando tengan un DTO.
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Aumentar el límite de tamaño de carga útil
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
